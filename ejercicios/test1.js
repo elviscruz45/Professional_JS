fe182ca79657f8e5b395e598eaf337516d0eb6d2
Elvis <elviscruz45@gmail.com>
2022-09-19 18:14:23 -0500

    finish

---
+++ ejercicios/test1.js
@@ -0,0 +1,13 @@
+myFunction();
+// code here can use carName as a global variable
+console.log(carName)
+
+function myFunction() {
+
+{var cama="cama"}
+console.log(cama)
+
+{let casa="casa"}
+console.log(casa)
+
+  carName = "Volvo";}
